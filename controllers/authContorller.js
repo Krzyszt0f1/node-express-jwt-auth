1467ed7cc1e11a1b3aca860b9732493052dbaf7d
Krzysztof Kasprzak <Krzysztof.Kasprzak@bglgroup.co.uk>
2021-11-17 17:42:29 +0000

    :construction: Adds error handling for user signup

--- controllers/authContorller.js
+++ controllers/authContorller.js
@@ -1,5 +1,26 @@
 const User = require('../models/User');
 
+// handle errors
+// within this function, err.code
+// will only be defined for unique property
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    const errors = { email: '', password: ''};
+    
+    // duplicate error code
+    if (err.code === 11000) {
+        errors.email = 'Email already in use';
+    }
+    
+    // validation errors
+     if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        })
+    }
+    return errors;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -14,10 +35,8 @@ module.exports.signup_post = async (req, res) => {
         const newUser = await User.create({email, password});
         res.status(201).json(newUser)
     } catch(err) {
-        console.log(err);
-        res.status(409).json({
-            message: err.message
-        });
+        const errors = handleErrors(err);
+        res.status(409).json({ errors });
     } 
     res.send('new signup');
 }
--- models/User.js
+++ models/User.js
@@ -1,16 +1,23 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
+// the second values in the arrays of required
+// are for creating custom error messages 
+// validate in the email is a custom validation
+// to check if it is an email
+// for some reason, unique has to be handled differently
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true,
-        minlength: 6
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Password has to be at least 6 characters long']
     }
 });
 
