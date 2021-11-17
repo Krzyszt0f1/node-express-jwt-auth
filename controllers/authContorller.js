6b9bb787837eef6f44d56cee41ebf93fea6535db
Krzysztof Kasprzak <Krzysztof.Kasprzak@bglgroup.co.uk>
2021-11-17 15:35:39 +0000

    :construction: Adds a User model and logic to handle signup post request

--- controllers/authContorller.js
+++ controllers/authContorller.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -6,7 +8,17 @@ module.exports.login_get = (req, res) => {
     res.render('login');
 }
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const newUser = await User.create({email, password});
+        res.status(201).json(newUser)
+    } catch(err) {
+        console.log(err);
+        res.status(409).json({
+            message: err.message
+        });
+    } 
     res.send('new signup');
 }
 
