e28d75d91078e1dc79c19a9cbd3edc8e6a160caa
Krzysztof Kasprzak <Krzysztof.Kasprzak@bglgroup.co.uk>
2021-11-18 16:27:52 +0000

    :construction: Adds logout button and the logic behind it using an authContorller, also adds conditional button rendering and user name rendering using custom middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const { requireAuth } = require('./custommidlleware/authMiddleware');
+const { requireAuth, checkUser } = require('./custommidlleware/authMiddleware');
  
 const app = express();
 
@@ -21,6 +21,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser); // wildcard applies it to every route
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
--- custommidlleware/authMiddleware.js
+++ custommidlleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt; // jwt is the custom name of the cookie we made
@@ -21,4 +22,28 @@ const requireAuth = (req, res, next) => {
     }
 }
 
-module.exports = { requireAuth };
+// check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token,
+            'here you should have a secret string that never gets published, otherwise, everything gets compromised',
+            async (err, decodedToken) => {
+                if(err) {
+                    console.log(err.message);
+                    res.locals.user = null;
+                    next();
+                } else {
+                    console.log(decodedToken);
+                    // research that res.locals stuff
+                    res.locals.user = await User.findById(decodedToken.id);
+                    next();
+                }
+            });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
